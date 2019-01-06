61efd2bb8db03f75d8cf4511cfed39a56a263520
Ango <ango.afnor@gmail.com>
2019-01-06 12:07:23 +0100

    Item management and display included in the Item class

--- classes.py
+++ classes.py
@@ -122,23 +122,23 @@ class Item:
     """
         Item class, basically
     """
-    def __init__(self, map_structure):
+    def __init__(self, map_structure, item):
         """
             Init
         """
-        self.aiguille_image = pygame.image.load(AIGUILLE).convert_alpha()
-        self.ether_image = pygame.image.load(ETHER).convert_alpha()
-        self.tube_image = pygame.image.load(TUBE).convert_alpha()
-        self.seringue_image = pygame.image.load(SERINGUE).convert_alpha()
+        self.image = pygame.image.load(item).convert_alpha()
+        self.syringe_image = pygame.image.load(SYRINGE).convert_alpha()
         self.map = map_structure
         self.pixel_x = 0
         self.pixel_y = 0
+        self.got_item = False
+
 
     def item_position(self, item):
         """
             Hey
         """
-        if item == "aiguille":
+        if item == "needle":
             ok_position = None
             while ok_position is None:
                 rand_ligne = randint(0, 14)
@@ -171,6 +171,21 @@ class Item:
                     ok_position = True
                     # We replace the sprite to ensure we cannot get 2 item on it
                     self.map[rand_ligne][rand_case] = "I"
+
+    def display(self, window, item, item_number):
+        """
+            Display management of items
+        """
+        if self.got_item is False:
+            window.blit(self.image, (self.position))
+        elif item_number == 3:
+            window.blit(self.syringe_image, [40, 450])
+        elif item == "needle":
+            window.blit(self.image, [0, 450])
+        elif item == "ether":
+            window.blit(self.image, [40, 450])
+        elif item == "tube":
+            window.blit(self.image, [80, 450])
 
     @property
     def position(self):
